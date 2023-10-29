a9ba73cf3ccbb94c686f3116eeda442efd19c2f8
Laura González <alu0101332819@ull.edu.es>
2023-10-29 13:44:49 +0000

    Añadiendo la clase SUVBMW.java

---
+++ src/main/java/org/ull/es/dap/brands/bmw/SUVBMW.java
@@ -0,0 +1,92 @@
+package org.ull.es.dap.brands.bmw;
+
+public class SUVBMW implements VehicleBMW {
+
+    private String brand;
+
+    private String type;
+    private String model;
+    private int year;
+    private String fuel;
+    double price;
+
+    private String imageURL;
+
+    public SUVBMW() {}
+
+    public SUVBMW(String brand, String type, String model, int year, String fuel, double price, String imageURL) {
+        this.brand = brand;
+        this.type = type;
+        this.model = model;
+        this.year = year;
+        this.fuel = fuel;
+        this.price = price;
+        this.imageURL = imageURL;
+    }
+
+    @Override
+    public String description() {
+        return  "Marca: " + brand + '\n' +
+                "Tipo de vehículo: " + type + '\n' +
+                "Modelo: " + model + '\n'+
+                "Año: " + year + '\n' +
+                "Combustible: " + fuel + '\n' +
+                "Precio: " + price + '\n';
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+}
